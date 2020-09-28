4ed956687dae0225b6535bb935c1efbe84ef730a
Tisham Dhar <whatnickd@gmail.com>
2020-09-29 09:22:03 +1000

    Fix needing manually export Working directory
    Signed-off-by: Tisham Dhar <whatnickd@gmail.com>

--- build_exe.bat
+++ build_exe.bat
@@ -1,6 +1,5 @@
-REM --SET VIEWER_DIR to working directory--
-REM --Run from Osgeo4W Shell--
 @echo off
+SET "VIEWER_DIR=%cd%"
 python3 -m pip install setuptools pyinstaller
 set PATH=C:\OSGeo4W64\apps\qgis\bin;%PATH%
 set PYTHONPATH=C:\OSGeo4W64\bin;C:\OSGeo4W64\apps\qgis\python;C:\OSGeo4W64\apps\qgis\python\plugins;C:\OSGeo4W64\apps\Python37\lib\site-packages;
