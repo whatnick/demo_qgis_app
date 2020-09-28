2d9cba810a18932e1d3a7e93885c0eced243135d
Tisham Dhar <whatnickd@gmail.com>
2020-09-28 12:39:02 +1000

    #1 Pack test data in built binary
    Signed-off-by: Tisham Dhar <whatnickd@gmail.com>

--- AirportViewer.spec
+++ AirportViewer.spec
@@ -1,11 +1,13 @@
 # -*- mode: python -*-
 import os
 block_cipher = None
+workdir = os.getenv('VIEWER_DIR')
 a = Analysis(['qgis_demo.py'],
-         pathex=[os.getenv('VIEWER_DIR')],
+         pathex=[workdir],
          binaries=None,
          datas=[('c:/OSGeo4W64/apps/qgis/plugins/*.dll','qgis_plugins'),
-                ('c:/OSGeo4W64/share/proj/*','proj_db')],
+                ('c:/OSGeo4W64/share/proj/*','proj_db'),
+                (os.path.join(workdir,'testdata/*'),'testdata')],
          hiddenimports=['PyQt5.QtSql','PyQt5.QtNetwork','PyQt5.QtXml','PyQt5.Qsci','PyQt5.QtPrintSupport'],
          hookspath=None,
          runtime_hooks=None,
--- qgis_demo.py
+++ qgis_demo.py
@@ -23,26 +23,29 @@ from qgis.gui import (
 # Invoke this script using "C:\OSGeo4W64\bin\python-qgis"
 # QgsApplication.setPrefixPath("C:\\OSGeo4W64\\apps\\qgis", True)
 
+
 def setup_qgis(qgs_app):
-    ''' Set QGIS paths based on whether running as a bundled application or not '''
-    if getattr(sys,'frozen',False):
+    """ Set QGIS paths based on whether running as a bundled application or not """
+    if getattr(sys, "frozen", False):
         print("Running In An Application Bundle")
         bundle_dir = sys._MEIPASS
         qgis_prefix_path = bundle_dir
-        qgis_plugin_path = bundle_dir + '\qgis_plugins'
-        qgis_proj_dir = bundle_dir + '\proj_db'
+        qgis_plugin_path = bundle_dir + "\qgis_plugins"
+        qgis_proj_dir = bundle_dir + "\proj_db"
         os.environ["PROJ_LIB"] = qgis_proj_dir
+        os.environ["TEST_DATA"] = bundle_dir + "/testdata/Australia_Airports.geojson"
     else:
         print("Running In A Normal Python Environment")
         bundle_dir = os.path.dirname(os.path.abspath(__file__))
         qgis_prefix_path = os.getenv("QGIS_PREFIX_PATH")
-        qgis_plugin_path = qgis_prefix_path + '\plugins'
+        qgis_plugin_path = qgis_prefix_path + "\plugins"
     qgs_app.setPrefixPath(qgis_prefix_path, True)
     qgs_app.setPluginPath(qgis_plugin_path)
     qgs_app.initQgis()
     registry = QgsProviderRegistry.instance()
-    if not 'ogr' in registry.providerList():
+    if not "ogr" in registry.providerList():
         print("ERROR: Missing OGR provider")
+
 
 # Create a reference to the QgsApplication.
 # Setting the second argument to True enables the GUI.  We need
@@ -58,17 +61,21 @@ canvas = QgsMapCanvas()
 canvas.setWindowTitle("Airport Viewer")
 canvas.show()
 
-vlayer = QgsVectorLayer('testdata/Australia_Airports.geojson', "Airports layer", "ogr")
+vlayer = QgsVectorLayer(
+    os.getenv("TEST_DATA", "testdata/Australia_Airports.geojson"),
+    "Airports layer",
+    "ogr",
+)
 if not vlayer.isValid():
     print("Vector layer failed to load!")
 
-urlWithParams = 'type=xyz&url=https://a.tile.openstreetmap.org/%7Bz%7D/%7Bx%7D/%7By%7D.png&zmax=19&zmin=0&crs=EPSG3857'
-rlayer = QgsRasterLayer(urlWithParams, 'OpenStreetMap', 'wms')  
+urlWithParams = "type=xyz&url=https://a.tile.openstreetmap.org/%7Bz%7D/%7Bx%7D/%7By%7D.png&zmax=19&zmin=0&crs=EPSG3857"
+rlayer = QgsRasterLayer(urlWithParams, "OpenStreetMap", "wms")
 
 if rlayer.isValid():
     QgsProject.instance().addMapLayer(rlayer)
 else:
-    print('Raster layer failed to load!')
+    print("Raster layer failed to load!")
 
 # add layer to the registry
 QgsProject.instance().addMapLayer(rlayer)
@@ -78,7 +85,7 @@ QgsProject.instance().addMapLayer(vlayer)
 canvas.setExtent(vlayer.extent())
 
 # set the map canvas layer set
-canvas.setLayers([vlayer,rlayer])
+canvas.setLayers([vlayer, rlayer])
 
 qgs.exec_()
 
